86130bea28586c85098d8b0eb9f73871fe20c0a7
shajahan-sayed <shajahansayed111@gmail.com>
2025-10-26 00:30:28 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description  = "selecting region here"
+  type = string
+}
+variable "ami_id" {
+  description = "selecting AMI ID"
+  type = string
+}
+variable "instance_type" {
+  description = "selecting instance type"
+  type = string
+  default = "t2.micro"
+}
+variable "key_name" {
+  description = "selecting key pair"
+  type = string 
+}
