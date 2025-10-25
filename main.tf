5d69c7bf0bf6e2e8f02e0cdf09628c91d2bfb9fe
shajahan-sayed <shajahansayed111@gmail.com>
2025-10-26 01:22:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,20 +9,20 @@ resource "aws_security_group" "terra" {
    from_port = 22
    to_port = 22
    protocol = "tcp"
-   cidr = ["0.0.0.0/0"]
+   cidr_blocks = ["0.0.0.0/0"]
  }
  ingress {
   description = "allowing ports"
   from_port = 80
   to_port = 80
   protocol = "tcp"
-  cidr = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
  }
  egress {
   from_port = 0
   to_port   = 0
   protocol = "-1"
-  cidr = ["0.0.0.0"]
+  cidr_blocks = ["0.0.0.0/0"]
  }
  tags = {
   name = "terra"
@@ -31,11 +31,11 @@ resource "aws_security_group" "terra" {
 
 #creating ec2 instance
 resource "aws_instance" "web" {
- ami_id = var.ami_id
+ ami = var.ami_id
  instance_type = var.instance_type
  key_name = var.key_name
  subnet_id = var.subnet_id
- vpc_security_group_id = [aws_security_group.terra.id]
+ vpc_security_group_ids = [aws_security_group.terra.id]
  associate_public_ip_address = true
 }
  
