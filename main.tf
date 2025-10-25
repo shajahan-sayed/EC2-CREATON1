95709455f61bacd29e740e8d0ba9e49a20bd9e2f
shajahan-sayed <shajahansayed111@gmail.com>
2025-10-26 01:07:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,6 +35,9 @@ resource "aws_instance" "web" {
  instance_type = var.instance_type
  key_name = var.key_name
  subnet_id = "var.subnet_id
+ vpc_security_group_ids = [aws_security_group.terra.id]
+ associate_public_ip_address = true
+}
  
  
  
