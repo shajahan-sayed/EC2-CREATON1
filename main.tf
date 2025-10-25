953a79a8fea06308dc79b4e914bcba2d23ea1903
shajahan-sayed <shajahansayed111@gmail.com>
2025-10-26 01:54:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "terra" {
 }
 
 #creating ec2 instance
-resource "aws_instance" "web" {
+resource "aws_instance" "webinar" {
  ami = var.ami_id
  instance_type = var.instance_type
  key_name = var.key_name
@@ -38,6 +38,8 @@ resource "aws_instance" "web" {
  vpc_security_group_ids = [aws_security_group.terra.id]
  associate_public_ip_address = true
 }
- 
+tags = {
+name = "webinar"
+}
  
  
